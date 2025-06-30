8d772faeca6de5b2ae7e5a5cca3356117dac2ee2
Wang Yuen <wangdemo2018@gmail.com>
2025-06-29 22:06:38 -0400

    fix issues

--- scripts-by-chapter/prepare-cloud-shell.sh
+++ scripts-by-chapter/prepare-cloud-shell.sh
@@ -1,8 +1,19 @@
 echo "---------- INSTALLING NANO ----------"
 sudo yum install nano -y
 
-echo "---------- INSTALLING EKSCTL ----------"
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/v0.147.0/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+
+
+# for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+# (Optional) Verify checksum
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
